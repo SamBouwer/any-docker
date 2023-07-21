fd93bd05ec680fe0f9e5bfbd87465133366dadae
SamBouwer <sam.bouwer@outlook.com>
2023-07-21 23:07:18 +0200

    Create startup.sh
    Initial commit to create network, then run coordinator, node, and filenode

---
+++ startup.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+echo any-sync-network create
+echo any-sync-coordinator -c /anytype/any-sync-tools/any-sync-coordinator.yml
+echo any-sync-node -c /anytype/any-sync-tools/any-sync-node.yml
+echo any-sync-filenode -c /anytype/any-sync-tools/any-sync-filenode.yml
