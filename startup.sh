e44f37da0c9703a968be6456ac4c9ed9fb400299
SamBouwer <sam.bouwer@outlook.com>
2023-07-21 23:37:08 +0200

    Update startup.sh
    Adjusted node file paths

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 any-sync-network create
-go run /anytype/any-sync-coordinator -c coordinator.yml
-go run /anytype/any-sync-node/any-sync-node -c sync_1.yml
-go run /anytype/any-sync-filenode -c file_1.yml
+go run /anytype/any-sync-coordinator/bin/any-sync-coordinator -c coordinator.yml
+go run /anytype/any-sync-node/bin/any-sync-node -c sync_1.yml
+go run /anytype/any-sync-filenode/bin/any-sync-filenode -c file_1.yml
 
 read -p "Press Enter to continue" </dev/tty
