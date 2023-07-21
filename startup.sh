74ba18fc9eca9cf9b53f2404b4735b1a66466d5e
SamBouwer <sam.bouwer@outlook.com>
2023-07-21 23:33:02 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 any-sync-network create
-go run any-sync-coordinator -c coordinator.yml
-any-sync-node/any-sync-node -c sync_1.yml
-bin/any-sync-filenode -c file_1.yml
+go run /anytype/any-sync-coordinator -c coordinator.yml
+go run /anytype/any-sync-node/any-sync-node -c sync_1.yml
+go run /anytype/any-sync-filenode -c file_1.yml
 
 read -p "Press Enter to continue" </dev/tty
