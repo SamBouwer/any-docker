fe867f35566e7d775918f04db0652af277459f64
SamBouwer <sam.bouwer@outlook.com>
2023-07-22 20:51:10 +0200

    Update startup.sh
    Removed wait before exit

--- startup.sh
+++ startup.sh
@@ -3,5 +3,3 @@ any-sync-network create
 /anytype/any-sync-coordinator/bin/any-sync-coordinator -c coordinator.yml
 /anytype/any-sync-node/bin/any-sync-node -c sync_1.yml
 /anytype/any-sync-filenode/bin/any-sync-filenode -c file_1.yml
-
-read -p "Press Enter to continue" </dev/tty
