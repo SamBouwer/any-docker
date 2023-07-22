d5ae1ed9111b73c77c7b616a1dff73cc75a40884
SamBouwer <sam.bouwer@outlook.com>
2023-07-22 20:20:10 +0200

    Update startup.sh
    Adjusted path to GO binary

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 any-sync-network create
-./any-sync-coordinator -c coordinator.yml
+./anytype/any-sync-coordinator/bin/any-sync-coordinator -c coordinator.yml
 go run /anytype/any-sync-node/bin/any-sync-node -c sync_1.yml
 go run /anytype/any-sync-filenode/bin/any-sync-filenode -c file_1.yml
 
