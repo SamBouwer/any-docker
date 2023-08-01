7e0ebdc50bfb3901f12d5811dcaa2d3d67e564a1
SamBouwer <sam.bouwer@outlook.com>
2023-08-01 20:58:20 +0200

    Update startup_node.sh

--- startup_node.sh
+++ startup_node.sh
@@ -10,7 +10,11 @@
 # echo -e "\r"
 # echo -e "\r"
 # echo -e "redis://redis:6379/?dial_timeout=3&db=1&read_timeout=6s&max_retries=2\r"
-echo starting any-sync-node...
+
+echo ""
+echo "-=[ starting any-sync-node... ]=-"
+echo ""
+
 /anytype/any-sync-node/bin/any-sync-node -c sync_1.yml &
 echo any-sync-node is running
 
