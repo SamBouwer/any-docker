78217e8d3d0bcc0b29f2cd2b41a7e47548e23e71
SamBouwer <sam.bouwer@outlook.com>
2023-08-01 09:23:28 +0200

    Update build_heart_clients.sh

--- build_heart_clients.sh
+++ build_heart_clients.sh
@@ -7,8 +7,8 @@ echo -= Build anytype-heart and anytype-clients =-
 workdir=${PWD}
 workdir=${workdir:-/}
 
-mkdir anytype
-cd anytype
+mkdir anytype-clients
+cd anytype-clients
 
 set -eux; \
         apt-get update; \
@@ -38,23 +38,19 @@ sdkmanager --sdk_root=/usr/lib/android-sdk/cmdline-tools/latest/bin --install "n
 mv -n /usr/lib/android-sdk/cmdline-tools/latest/bin/ndk/23.2.8568313/ /usr/lib/android-sdk/ndk-bundle/
 
 # Build any-heart including protobuf files and test dependencies
-cd $workdir/anytype
+cd $workdir/anytype-clients
 git clone https://github.com/anyproto/anytype-heart
-heart.yml .
-cd anytype/any-heart
+cd anytype-clients/any-heart
 make test-deps
 make test
 make setup-protoc
 make protos
-mkdir ../anytype-ts
+mkdir -p $workdir/anytype-clients/anytype-ts
 #mkdir ../anytype-ts/dist
 #mkdir ../anytype-ts/dist/lib
-make install-dev-js ANY_SYNC_NETWORK=/anytype/heart.yml
+make install-dev-js ANY_SYNC_NETWORK=$workdir/heart.yml
 # I don't have a machine running MacOS so I cannot setup the deps for iOS
 #make build-ios ANY_SYNC_NETWORK=/anytype/heart.yml
 # I can't get the Android version to properly build
 #make build-android ANY_SYNC_NETWORK=/anytype/heart.yml
 make protos-java
-
-cd anytype/any-heart/anytype-ts
-chmod +x any-heart
