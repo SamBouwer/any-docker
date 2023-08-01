b13a731ca85120db8d13d29d83bfd980bf287b67
SamBouwer <sam.bouwer@outlook.com>
2023-08-01 14:12:08 +0200

    Update setup_dev_env.sh

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -14,7 +14,8 @@ export GOPATH=$HOME/go
 export GOROOT=/usr/local/go
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 export ANDROID_SDK_ROOT=/usr/lib/android-sdk
-export ANDROID_HOME=/usr/lib/android-sdk/
+export ANDROID_HOME=/usr/lib/android-sdk
+export PATH=$ANDROID_HOME/cmdline-tools/bin:$PATH
 
 . ~/.bashrc
 
