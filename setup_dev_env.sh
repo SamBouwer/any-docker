3ae0c5ba6b0ab1b6b096c1314b762ff211e243db
SamBouwer <sam.bouwer@outlook.com>
2023-08-01 22:09:17 +0200

    Update setup_dev_env.sh

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -44,21 +44,21 @@ sudo dpkg --add-architecture i386
 set -eux; \
         sudo apt-get update; \
         sudo apt-get install -y --no-install-recommends \
-                git \ # needed for downloading dependencies from git repositories
-                openssl \ # needed to create certificates to communicate securely with remote servers to download files
-                protobuf-compiler \ # needed for building middleware libraries
-                libprotoc-dev \ # needed for building middleware libraries
-                android-sdk \ # needed for building middleware libraries for Android
-                unzip \ # needed for unzipping downloads
-                npm \ # needed for building the desktop client
-                libsecret-1-dev \ # needed for ...
-                jq \ # needed for ...
-                wine \ # needed for building the desktop client for Windows
-                wine32 \ # needed for building the desktop client for Windows
-                docker-ce \ # needed for building and running docker images
-                docker-ce-cli \ # needed for building and running docker images
-                containerd.io \ # needed for building and running docker images
-                docker-buildx-plugin \ # needed for building and running docker images
-                docker-compose-plugin \ # needed for building and running docker images
+                git `# needed for downloading dependencies from git repositories` \
+                openssl `# needed to create certificates to communicate securely with remote servers to download files` \
+                protobuf-compiler `# needed for building middleware libraries` \
+                libprotoc-dev `# needed for building middleware libraries` \
+                android-sdk `# needed for building middleware libraries for Android` \
+                unzip `# needed for unzipping downloads` \
+                npm `# needed for building the desktop client` \
+                libsecret-1-dev `# needed for ...` \
+                jq `# needed for ...` \
+                wine `# needed for building the desktop client for Windows` \
+                wine32 `# needed for building the desktop client for Windows` \
+                docker-ce `# needed for building and running docker images` \
+                docker-ce-cli `# needed for building and running docker images` \
+                containerd.io `# needed for building and running docker images` \
+                docker-buildx-plugin `# needed for building and running docker images` \
+                docker-compose-plugin `# needed for building and running docker images` \
         ; \
         sudo rm -rf /var/lib/apt/lists/*
