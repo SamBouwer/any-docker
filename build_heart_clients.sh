d4a74a0e9df4e0e3cfd286680443db51b4f00e61
SamBouwer <sam.bouwer@outlook.com>
2023-08-01 14:12:06 +0200

    Update build_heart_clients.sh

--- build_heart_clients.sh
+++ build_heart_clients.sh
@@ -16,9 +16,7 @@ unzip -qo cmdtools.zip
 cd cmdline-tools
 mkdir -p latest
 mv -n bin/ latest/
-export ANDROID_HOME=/usr/lib/android-sdk
-export ANDROID_SDK_ROOT=/usr/lib/android-sdk
-export PATH=$ANDROID_HOME/cmdline-tools/bin:$PATH
+
 cd
 mkdir -p /usr/lib/android-sdk/ndk-bundle
 sdkmanager --sdk_root=/usr/lib/android-sdk/cmdline-tools/latest/bin --install "ndk;23.2.8568313"
