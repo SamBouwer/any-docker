eb4908272e78da8d7bbf4f8cff4ed4f3efb1dc4a
SamBouwer <sam.bouwer@outlook.com>
2023-08-01 21:31:43 +0200

    Update setup_dev_env.sh

--- setup_dev_env.sh
+++ setup_dev_env.sh
@@ -15,13 +15,10 @@ done
 
 GOLANG_VER_INSTALLED=$(go version)
 
-echo ${GOLANG_VER_INSTALLED}
-echo "go version go${GOLANG_VER} linux/amd64"
-
 if [ "${GOLANG_VER_INSTALLED}" == "go version go${GOLANG_VER} linux/amd64" ] ; then
-  echo "Go is already installed, skipping"
+  echo "'go version go${GOLANG_VER} linux/amd64' is already installed, skipping..."
 else
-  echo "Installing Go"
+  echo "Installing 'go version go${GOLANG_VER} linux/amd64'"
 fi
 
 exit
