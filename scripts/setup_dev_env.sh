921521a1ef8c49abe1b2c413c50c7b10510de6aa
Sam Bouwer <sam.bouwer@outlook.com>
2023-08-04 17:53:06 +0000

    Reorganized files into folders

---
+++ scripts/setup_dev_env.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+# This script installs all packages and dependencies required to build Anytype nodes and clients. 
+# Run this script at least once before using the scripts or make commands to build your own nodes and/or clients.
+
+echo ""
+echo "-=[ Setting up development environment for building anytype nodes and clients ]=-"
+echo ""
+
+workdir=${PWD}
+workdir=${workdir:-/}
+
+mkdir -p $workdir/anytype-clients
+
+while getopts v: flag
+do
+    case "${flag}" in
+        v) GOLANG_VER=${OPTARG};;
+    esac
+done
+
+GOLANG_VER_INSTALLED=$(go version)
+
+if [ "${GOLANG_VER_INSTALLED}" == "go version go${GOLANG_VER} linux/amd64" ] ; then
+  echo "'go version go${GOLANG_VER} linux/amd64' is already installed, skipping..."
+else
+  echo "Installing 'go version go${GOLANG_VER} linux/amd64'"
+  wget -N https://go.dev/dl/go${GOLANG_VER}.linux-amd64.tar.gz
+  tar -C /usr/local -xzf $HOME/go${GOLANG_VER}.linux-amd64.tar.gz
+  rm go${GOLANG_VER}.linux-amd64.tar.gz
+fi
+
+
+#Add directories to variables
+
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> ~/.bashrc
+echo 'export ANDROID_SDK_ROOT=/usr/lib/android-sdk' >> ~/.bashrc
+echo 'export ANDROID_HOME=/usr/lib/android-sdk' >> ~/.bashrc
+echo 'export PATH=$ANDROID_HOME/cmdline-tools/bin:$PATH' >> ~/.bashrc
+
+source ~/.bashrc
+
+sudo dpkg --add-architecture i386 
+
+set -eux; \
+        sudo apt-get update; \
+        sudo apt-get install -y --no-install-recommends \
+                git `# needed for downloading dependencies from git repositories` \
+                openssl `# needed to create certificates to communicate securely with remote servers to download files` \
+                protobuf-compiler `# needed for building middleware libraries` \
+                libprotoc-dev `# needed for building middleware libraries` \
+                android-sdk `# needed for building middleware libraries for Android` \
+                unzip `# needed for unzipping downloads` \
+                npm `# needed for building the desktop client` \
+                libsecret-1-dev `# needed for ...` \
+                jq `# needed for ...` \
+                wine `# needed for building the desktop client for Windows` \
+                wine32 `# needed for building the desktop client for Windows` \
+                docker-ce `# needed for building and running docker images` \
+                docker-ce-cli `# needed for building and running docker images` \
+                containerd.io `# needed for building and running docker images` \
+                docker-buildx-plugin `# needed for building and running docker images` \
+                docker-compose-plugin `# needed for building and running docker images` \
+                rpm `# needed for building Anytype Desktop for Linux` \
+        ; \
+        sudo rm -rf /var/lib/apt/lists/*
+
+sudo snap install snapcraft --classic
---
+++ scripts/startup_node.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# commented out the line below because docker is not designed to run scripts interactively inside a container. The configuration yml files need to be created in advance.
+# any-sync-network create
+# echo -e "\r"
+# echo -e "mongodb://mongorootuser:mongorootpassword@mongo:27017\r"
+# echo -e "\r"
+# echo -e "\r"
+# echo -e "192.168.1.175:9000\r"
+# echo -e "\r"
+# echo -e "\r"
+# echo -e "\r"
+# echo -e "redis://redis:6379/?dial_timeout=3&db=1&read_timeout=6s&max_retries=2\r"
+
+echo ""
+echo "-=[ starting any-sync-node... ]=-"
+echo ""
+
+/anytype/any-sync-node/bin/any-sync-node -c sync_1.yml &
+echo any-sync-node is running
+
+read -p "Press Enter to continue" </dev/tty
