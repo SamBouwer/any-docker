06e44230c29382ebe5d8c7b2d5b63f83e344e033
SamBouwer <sam.bouwer@outlook.com>
2023-08-05 19:20:19 +0200

    Update setup_dev_env.sh
    Fixed permission issue to write to /usr/local

--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -25,7 +25,7 @@ if [ "${GOLANG_VER_INSTALLED}" == "go version go${GOLANG_VER} linux/amd64" ] ; t
 else
   echo "Installing 'go version go${GOLANG_VER} linux/amd64'"
   wget -N https://go.dev/dl/go${GOLANG_VER}.linux-amd64.tar.gz -P $HOME/
-  tar -C /usr/local -xzf $HOME/go${GOLANG_VER}.linux-amd64.tar.gz
+  sudo tar -C /usr/local -xzf $HOME/go${GOLANG_VER}.linux-amd64.tar.gz
   rm $HOME/go${GOLANG_VER}.linux-amd64.tar.gz
 fi
 
